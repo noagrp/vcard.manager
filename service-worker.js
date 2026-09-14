373232bc9cbd5eee23e4c6e676d88ca83cd12c63
noagrp <lihwlee11@gmail.com>
2026-09-15 02:50:25 +0800

    Refresh vCard Manager PWA cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='vcard-manager-v1';
+const CACHE='vcard-manager-v2';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./favicon-32x32.png','./apple-touch-icon.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
